97caade268036f640ef032946e208e6f44d0ba4e
Daria Dubovka <ddubovka@gmail.com>
2019-04-13 09:25:52 +0300

    Rename value price

--- src/ru/dubovka/task1/GasPrice.java
+++ src/ru/dubovka/task1/GasPrice.java
@@ -1,11 +1,11 @@
 package ru.dubovka.task1;
 
 public class GasPrice {
-    static double PRICE;
+    static double price;
     static double litre;
 
     public static void main(String[] args){
-        System.out.println(PRICE*litre);
+        System.out.println(price * litre);
 
     }
 }
