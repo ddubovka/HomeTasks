b6d4f13bf08ebd1653caa4f358086d6eff248b67
ddubovka <47240145+ddubovka@users.noreply.github.com>
2019-04-14 21:20:20 +0300

    Delete Hours.java

--- src/ru/dubovka/task1/Hours.java
+++
@@ -1,9 +0,0 @@
-package ru.dubovka.task1;
-
-public class Hours {
-    public static void main(String[] args) {
-        int sec = 50000;
-        int result = sec / 3600;
-        System.out.println(result);
-    }
-}
