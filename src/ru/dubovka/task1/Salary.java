1eccb48ad3738881d740ba0bdce36f541f1702f0
ddubovka <47240145+ddubovka@users.noreply.github.com>
2019-04-14 21:20:28 +0300

    Delete Salary.java

--- src/ru/dubovka/task1/Salary.java
+++
@@ -1,9 +0,0 @@
-package ru.dubovka.task1;
-
-public class Salary {
-    public static void main(String[] args) {
-        int salaryGross = 80000;
-        int salaryFinal = salaryGross * 87 / 100;
-        System.out.println(salaryFinal);
-    }
-}
