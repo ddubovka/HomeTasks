0dc6e3d64152667d36135b5cfa9979af5739c336
Daria Dubovka <ddubovka@gmail.com>
2019-04-14 21:46:22 +0300

    file's return

---
+++ src/ru/dubovka/task1/GasPrice.java
@@ -0,0 +1,10 @@
+package ru.dubovka.task1;
+
+public class GasPrice {
+    public static void main(String[] args) {
+        int price = 49;
+        int litre = 50;
+        int result = price * litre;
+        System.out.println(result);
+    }
+}
---
+++ src/ru/dubovka/task1/Hours.java
@@ -0,0 +1,9 @@
+package ru.dubovka.task1;
+
+public class Hours {
+    public static void main(String[] args) {
+        int sec = 50000;
+        int result = sec / 3600;
+        System.out.println(result);
+    }
+}
