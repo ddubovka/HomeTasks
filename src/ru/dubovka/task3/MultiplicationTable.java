8a6b1cd885b24f51250a8167a7ce3ddbd5a9dc05
Daria Dubovka <ddubovka@gmail.com>
2019-04-17 23:26:16 +0300

    Task3.1-3. Multiplication table without scanner

--- src/ru/dubovka/task3/MultiplicationTable.java
+++ src/ru/dubovka/task3/MultiplicationTable.java
@@ -1,23 +1,15 @@
 package ru.dubovka.task3;
 
-import java.util.Scanner;
+
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        Scanner table = new Scanner(System.in);
-        System.out.println("Insert number from 1 to 9:");
-        int integer = table.nextInt();
-        while (integer > 9 || integer < 1) {
-            System.out.println("Try another number");
-            System.out.println("Insert number from 1 to 9:");
-            integer = table.nextInt();
-            if (integer < 10 && integer > 1) {
-                break;
-            }
-        }
         System.out.println("Multiplication table:");
-        for (int i = 1; i < 10; i++) {
-            System.out.println(integer + " * " + i + " = " + integer * i);
+        for (int i = 1; i < 10; ++i) {
+            for (int c = 1; c < 11; ++c) {
+                System.out.print(c * i + " ");
+            }
+            System.out.println();
         }
     }
 }
